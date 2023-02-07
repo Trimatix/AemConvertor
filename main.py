77e3c1a6dd121598c96720d1ae8319b5561270cb
Trimatix <1JasperLaw@gmail.com>
2023-02-07 20:53:23 +0000

    Use shared reader functions

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
+from typing import BinaryIO, Tuple
 from tqdm import tqdm
 import struct
 from numpy import float32
+
+def readShort(f: BinaryIO) -> int:
+    return struct.unpack("h", f.read(2))[0]
+
+def readFloat(f: BinaryIO) -> float:
+    return struct.unpack("f", f.read(4))[0]
+
+def readFloats(f: BinaryIO, num: int) -> Tuple[float, ...]:
+    return tuple(struct.unpack("f", f.read(4))[0] for _ in range(num))
 
 if __name__ == '__main__':
     option = int(input("Please choose the feature ( 1.aem2obj | 2.obj2aem ): "))
@@ -18,7 +28,7 @@ if __name__ == '__main__':
         file_aem = open(file_in, 'rb')
         file_obj = open(file_out, 'a')
         file_aem.seek(24)
-        v_num = struct.unpack("h", file_aem.read(2))[0]
+        v_num = readShort(file_aem)
         s = f'# Vertices {v_num}\n'
         file_obj.write(s)
         print(s)
@@ -28,9 +38,9 @@ if __name__ == '__main__':
         v_z = []
         i = 0
         for i in tqdm(range(v_num)):
-            v_x.append(float32(struct.unpack("f", file_aem.read(4))[0]))
-            v_y.append(float32(struct.unpack("f", file_aem.read(4))[0]))
-            v_z.append(float32(struct.unpack("f", file_aem.read(4))[0]))
+            v_x.append(readFloat(file_aem))
+            v_y.append(readFloat(file_aem))
+            v_z.append(readFloat(file_aem))
             s = f'v  {v_x[i]} {v_y[i]} {v_z[i]}\n'
             file_obj.write(s)
         s = f'\n# UVs {v_num}\n'
@@ -40,8 +50,8 @@ if __name__ == '__main__':
         vt_y = []
         i = 0
         for i in tqdm(range(v_num)):
-            vt_x.append(float32(struct.unpack("f", file_aem.read(4))[0]))
-            vt_y.append(float32(struct.unpack("f", file_aem.read(4))[0]))
+            vt_x.append(readFloat(file_aem))
+            vt_y.append(readFloat(file_aem))
             s = f'vt  {vt_x[i]} {vt_y[i]}\n'
             file_obj.write(s)
         s = f'\n# Normals {v_num}\n'
@@ -51,9 +61,9 @@ if __name__ == '__main__':
         vn_y = []
         vn_z = []
         for i in tqdm(range(v_num)):
-            vn_x.append(float32(struct.unpack("f", file_aem.read(4))[0]))
-            vn_y.append(float32(struct.unpack("f", file_aem.read(4))[0]))
-            vn_z.append(float32(struct.unpack("f", file_aem.read(4))[0]))
+            vn_x.append(readFloat(file_aem))
+            vn_y.append(readFloat(file_aem))
+            vn_z.append(readFloat(file_aem))
             s = f'vn  {vn_x[i]} {vn_y[i]} {vn_z[i]}\n'
             file_obj.write(s)
         s = f'\n# Faces {v_num // 3}\n'
